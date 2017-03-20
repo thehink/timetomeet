bc96ca1f7abb2d66d3fa2255de473e9a42f4bae5
Thehink <benja280@gmail.com>
2017-03-20 23:56:45 +0100

    use localization file

--- src/components/FacilityBeverage/FacilityBeverage.jsx
+++ src/components/FacilityBeverage/FacilityBeverage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import venueData from '../../data/venue_data.json';
+import lang from '../../lang/sv.json';
 
 export default class FacilityBeverage extends React.Component {
 
@@ -9,13 +10,13 @@ export default class FacilityBeverage extends React.Component {
   }
 
   renderListItem(item){
-    return (<li key={`food_${item.id}`}>{item.name}</li>);
+    return (<li key={`food_${ item.id }`}>{ item.name }</li>);
   }
 
   render () {
     return (
       <div className="facility">
-        <p className="name">Name</p>
+        <p className="name">{ lang.venueFoodBeverage }</p>
         <ul>
           {this.state.tags.map(this.renderListItem)}
         </ul>
