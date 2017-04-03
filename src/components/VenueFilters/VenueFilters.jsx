0af50a854ab87e8ddd4aa951d1bd0609ecb09c91
thehink <benja280@gmail.com>
2017-04-03 14:02:59 +0200

    added component VenueFilters

---
+++ src/components/VenueFilters/VenueFilters.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './VenueFilters.scss';
+
+export default class Venue extends React.Component {
+  render () {
+    return (
+      <div className="venue-filters">
+
+      </div>
+    );
+  }
+}
