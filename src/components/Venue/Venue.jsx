a7acfd7e6ba0ac0cc6508d70c76479be52d93474
thehink <benja280@gmail.com>
2017-04-04 16:07:19 +0200

    updated venue filters

--- src/components/Venue/Venue.jsx
+++ src/components/Venue/Venue.jsx
@@ -13,11 +13,11 @@ export default class Venue extends React.Component {
 				<div className="row">
 					<div className="col-xs-12 col-md-8 col-lg-9">
 						<VenueImage />
+						<VenueFilters />
 					</div>
 					<div className="col-xs-12 col-md-4 col-lg-3">
 						<Facilities />
 					</div>
-					<VenueFilters />
 				</div>
       </div>
     );
--- src/components/VenueFilters/VenueFilters.jsx
+++ src/components/VenueFilters/VenueFilters.jsx
@@ -3,11 +3,21 @@ import React from 'react'
 import './VenueFilters.scss';
 
 export default class VenueFilters extends React.Component {
+
+	componentDidMount(){
+		$(".venue-filters").sticky();
+	}
+
   render () {
     return (
-      <div className="venue-filters">
-				VenueFilters :O
-      </div>
+			<div className="venue-filters-wrapper">
+				<div className="venue-filters">
+					<h1 className="name">{ venueData.plantName }</h1>
+					<p className="place">
+						{`${venueAddress.street} ${venueAddress.city.name}`}
+					</p>
+	      </div>
+			</div>
     );
   }
 }
