a52a38feb42873481fe9e71b2e5b07e0ba80952d
thehink <benja280@gmail.com>
2017-04-04 15:01:43 +0200

    change container to container-fluid

--- src/components/Venue/Venue.jsx
+++ src/components/Venue/Venue.jsx
@@ -9,12 +9,12 @@ import './Venue.scss';
 export default class Venue extends React.Component {
   render () {
     return (
-      <div className="venue container">
+      <div className="venue container-fluid">
 				<div className="row">
-					<div className="col-xs-12 col-md-8">
+					<div className="col-xs-12 col-md-8 col-lg-9">
 						<VenueImage />
 					</div>
-					<div className="col-xs-12 col-md-4">
+					<div className="col-xs-12 col-md-4 col-lg-3">
 						<Facilities />
 					</div>
 					<VenueFilters />
