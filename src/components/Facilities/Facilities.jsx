22f1273febbe247b6320c546f116c69019c49d4b
carlaberg <mail.carl.aberg@gmail.com>
2017-04-04 14:12:03 +0200

    Fixed container width

--- src/components/Facilities/Facilities.jsx
+++ src/components/Facilities/Facilities.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 
-
+import "./Facilities.scss";
 export default class Facilities extends React.Component
 {
   render () {
     return (
-			<div className="facilities container">
+			<div className="facilities">
 
 				<div className="row">
 
 					<div className="col-md-12"><h1>FACILITETER</h1></div>
 
-					<div className="col-md-12"><h2>Sittningsalternativ</h2></div>
+					<div className="col-md-12">
+
+						<h2>Sittningsalternativ</h2>
+
+						<p>Bio  Skolsittning  Styrelsesittning  U-Bord</p>
+
+					</div>
+
+					<hr />
 
 					<div className="col-md-12"><h2>Utrustning</h2></div>
 
