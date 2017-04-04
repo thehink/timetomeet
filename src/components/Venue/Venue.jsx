b152ae4c96674c0d55443ef85e7ec70906ebb4b6
thehink <benja280@gmail.com>
2017-04-04 14:56:30 +0200

    updated component

--- src/components/Venue/Venue.jsx
+++ src/components/Venue/Venue.jsx
@@ -4,15 +4,17 @@ import VenueImage from '../VenueImage';
 import Facilities from '../Facilities';
 import VenueFilters from '../VenueFilters';
 
+import './Venue.scss';
+
 export default class Venue extends React.Component {
   render () {
     return (
       <div className="venue container">
 				<div className="row">
-					<div className="col-md-8">
+					<div className="col-xs-12 col-md-8">
 						<VenueImage />
 					</div>
-					<div className="col-md-4">
+					<div className="col-xs-12 col-md-4">
 						<Facilities />
 					</div>
 					<VenueFilters />
--- src/components/VenueImage/VenueImage.jsx
+++ src/components/VenueImage/VenueImage.jsx
@@ -3,10 +3,28 @@ import React from 'react'
 import './VenueImage.scss';
 
 export default class VenueImage extends React.Component {
+
+	state = {
+		images: venueData.plantImages
+	}
+
+	componentDidMount(){
+
+	}
+
+	renderItem(item){
+    return (
+      <li key={`slide_${item.id}`} style={{backgroundImage: `url(${item.url})`}}></li>
+    );
+  }
+
   render () {
+
+		//${this.state.images[0].url}
     return (
       <div className="venue-image">
-				VenueImage
+				<div className="venue-image_image" style={ {backgroundImage: `url(https://be.timetomeet.se/static/crb/media/20160913/BjorkbackeFotografi_-_Matsal.jpg)`} }>
+				</div>
       </div>
     );
   }
