d976e57504352ed9b40857fac515e7a17c446510
carlaberg <mail.carl.aberg@gmail.com>
2017-04-10 13:47:24 +0200

    Addded changes to roomcard

--- src/components/RoomInfo/RoomInfo.jsx
+++ src/components/RoomInfo/RoomInfo.jsx
@@ -2,6 +2,9 @@ import React from 'React';
 
 import "./RoomInfo.scss";
 
+
+import Projector from "../../../public/assets/images/new_icons/inlineIcons.jsx";
+
 export default class RoomInfo extends React.Component {
 
 	renderItem(item) {
@@ -9,14 +12,37 @@ export default class RoomInfo extends React.Component {
 			 <li key={item.id}>{item.name}</li>
 		)
 	}
+	renderFacilities(item) {
+
+		let iconUrl;
+
+		switch(item.name) {
+			case "Projektor":
+			iconUrl = "assets/images/new_icons/projektor.svg";
+			break;
+		}
+
+
+
+		return (
+
+
+
+			 <li key={item.id}>{item.name} <div className="facility-icon" style={{backgroundImage: `url(${iconUrl}`}}></div></li>
+		)
+	}
 
 
 	render() {
 
 
+
+
 		return (
 
 			<div className="room-info">
+
+				<Projector />
 
 				<h1>{this.props.info.name}</h1>
 
@@ -25,7 +51,7 @@ export default class RoomInfo extends React.Component {
 				<h2>Faciliteter</h2>
 
 				<ul className="tech-list">
-					{this.props.info.tech.map(this.renderItem)}
+					{this.props.info.tech.map(this.renderFacilities)}
 				</ul>
 
 				<hr />
