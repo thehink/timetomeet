fcf53942c348053f6919452057e8f30ba8958d0f
carlaberg <mail.carl.aberg@gmail.com>
2017-04-11 13:37:34 +0200

    Fixed responsivenes column height

--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -19,15 +19,18 @@ state = {imgHeight: "auto", buttonDivHeight: "auto"}
 
 		this.setState({buttonDivHeight: (this.room_card.clientHeight + 28).toString() + "px"});
 
-		if(window.innerWidth > 500) {
+		if(window.innerWidth > 768) {
 			this.setState({
 				imgHeight: (this.room_card.clientHeight + 28).toString() + "px",
-				initialHeight: (this.room_card.clientHeight + 28).toString() + "px"
+				initialHeight: (this.room_card.clientHeight + 28).toString() + "px",
+				buttonDivHeight: (this.room_card.clientHeight + 28).toString() + "px"
+
 			});
 		} else {
 			this.setState({
 				imgHeight: "200px",
-				initialHeight: this.room_card.clientHeight.toString() + "px"
+				initialHeight: this.room_card.clientHeight.toString() + "px",
+				buttonDivHeight: "auto"
 			});
 		}
 
@@ -40,11 +43,14 @@ state = {imgHeight: "auto", buttonDivHeight: "auto"}
 
 	handleResize() {
 
-		if(window.innerWidth < 500) {
-			this.setState({imgHeight: "200px"});
+		if(window.innerWidth < 768) {
+			this.setState({imgHeight: "200px", buttonDivHeight: "auto"});
 		} else {
-			this.setState({parentHeight: window.getComputedStyle(this.room_card, null).getPropertyValue("height"), imgHeight: this.state.parentHeight})
-			this.setState({imgHeight: this.state.parentHeight});
+			this.setState({
+				parentHeight: window.getComputedStyle(this.room_card, null).getPropertyValue("height"),
+				imgHeight: this.state.parentHeight,
+				buttonDivHeight: this.room_card.clientHeigh.toString() + "px"
+			});
 		}
 
 
@@ -61,7 +67,7 @@ state = {imgHeight: "auto", buttonDivHeight: "auto"}
 				<div ref={input => this.room_card = input} className="col-md-5 col-sm-6 col-xs-12">
 					<RoomInfo info={this.props.room} />
 				</div>
-				<div className="col-md-3 col-sm-12 col-xs-12" style={{height: this.state.imgHeight}}>
+				<div className="col-md-3 col-sm-12 col-xs-12" style={{height: this.state.buttonDivHeight}}>
 					<RoomSelect room={ this.props.room } />
 				</div>
 			</div>
