395bc44d8e6fc40d370e6cbf78220d1aee860201
thehink <benja280@gmail.com>
2017-04-11 10:59:39 +0200

    no image will now display default image

--- src/components/RoomImage/RoomImage.jsx
+++ src/components/RoomImage/RoomImage.jsx
@@ -6,10 +6,11 @@ export default class RoomImage extends React.Component {
 
 	render() {
 
+		let image = this.props.image || '/assets/images/noimg.gif';
 
 		return (
 
-			<div className="room-image" style={{backgroundImage: `url(${this.props.image}`}}>
+			<div className="room-image" style={{backgroundImage: `url(${image}`}}>
 
 			</div>
 		)
